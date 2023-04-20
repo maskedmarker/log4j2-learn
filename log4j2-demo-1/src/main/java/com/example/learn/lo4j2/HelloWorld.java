9279fd127c9bb6720a231a8a23e97aad103db6ca
maskedmarker <changjianghui0822@gmail.com>
2023-04-20 22:46:35 +0800

    修改 [日志输出格式]

--- log4j2-demo-1/src/main/java/com/example/learn/lo4j2/HelloWorld.java
+++ log4j2-demo-1/src/main/java/com/example/learn/lo4j2/HelloWorld.java
@@ -3,6 +3,9 @@ package com.example.learn.lo4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * log4j2.xml中Configuration.status用来控制log4j内部实现的日志级别,有别于应用的日志级别
+ */
 public class HelloWorld {
     private static final Logger logger = LogManager.getLogger(HelloWorld.class);
 
